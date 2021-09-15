cbc383c554009c4fd5f6635a28817a85d25c6149
chaoyue.xue <zhishuichengxin@gmail.com>
2021-09-15 15:37:50 +0800

    add ====

--- scripts/check-branch.js
+++ scripts/check-branch.js
@@ -4,6 +4,7 @@ const projectPath = path.resolve(__dirname, '../')
 const git = SimpleGit(projectPath);
 
 const initMethod = async () => {
+  console.log('=================')
   const currentBranch = await git.branch(['--no-merged'])
   console.log(currentBranch);
 }
