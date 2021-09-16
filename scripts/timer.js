9e23a4d3c7868f207fda37939592c7d11de1902b
chaoyue.xue <zhishuichengxin@gmail.com>
2021-09-16 12:07:49 +0800

    fix: change visible

--- scripts/timer.js
+++ scripts/timer.js
@@ -4,6 +4,26 @@ const projectPath = path.resolve(__dirname, '../')
 const git = SimpleGit(projectPath);
 var release = require('github-pr-release');
 
+
+const pr = async () => {
+
+    // pull request
+    const config = {
+      token: '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',
+      owner: 'chaoyuexue',
+      repo:  'interval-push',
+      base: 'develop1',
+      head: 'master'
+    };
+    try {
+      release(config).then(res => {
+        console.log(res);
+      }).catch(e => console.log(e, '=========='));
+    } catch (error) {
+      console.log(error);
+    }
+}
+
 const intervalCommit = async () => {
   const logs = await git.raw('cherry')
   const remoteNameOrigin = await git.remote()
@@ -16,19 +36,11 @@ const intervalCommit = async () => {
     const currentCommit = needPushedArr[0];
     const result = await git.push([remoteName, `${currentCommit}:${currentBranch.current}`]);
     console.log(result);
-
-    // pull request
-    const config = {
-      token: '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',
-      owner: 'chaoyuexue',
-      repo:  'interval-push',
-    };
-    const prResult = await release(config);
-    console.log(prResult);
   }
   // process.exit(1);
 }
+pr()
 
 // setInterval(() => {
-  intervalCommit()
+  // intervalCommit()
 // }, 60000);
